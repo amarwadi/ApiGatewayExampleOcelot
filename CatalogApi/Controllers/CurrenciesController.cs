b8bfa1bbdb84e22fdb1e6ef56affa4ecb23a952e
Anup Marwadi <amarwadi@gmail.com>
2018-09-19 16:11:50 -0700

    Adding initial checkin

---
+++ CatalogApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogApi.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/currencies
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "usd", "inr", "jpy" };
+        }
+    }
+
+
+
+}
